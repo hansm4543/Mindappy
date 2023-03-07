d3b313dc8b8e900fe06336e7638f54add2d4d68e
Hans-Märten Liiu <hm4543@tlu.ee>
2023-03-07 11:56:18 +0200

    async storage first trials

--- App.js
+++ App.js
@@ -4,11 +4,31 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BottomTabNavigator from "./src/Navigation/BottomTabNavigator";
 import StartScreen from "./src/StartScreen";
 import QuestionScreen from "./src/QuestionScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem("@storage_Key", jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem("@storage_Key");
+    const returnValue = jsonValue != null ? JSON.parse(jsonValue) : null;
+    return returnValue;
+  } catch (e) {
+    // error reading value
+  }
+};
+
 function App() {
-  let isFirstStartUp = false;
+  let isFirstStartUp = true;
   const InitialRoute = isFirstStartUp ? "QuestionScreen" : "Bottomtab";
 
   return (
@@ -20,7 +40,11 @@ function App() {
         })}
       >
         <Stack.Screen name="StartScreen" component={StartScreen} />
-        <Stack.Screen name="QuestionScreen" component={QuestionScreen} />
+        <Stack.Screen
+          name="QuestionScreen"
+          component={QuestionScreen}
+          initialParams={{ storeData, getData }}
+        />
         <Stack.Screen
           name="Bottomtab"
           component={BottomTabNavigator}
--- src/QuestionScreen.js
+++ src/QuestionScreen.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, View, Image, Text } from "react-native";
 
-function StartScreen({ navigation }) {
+function StartScreen({ navigation, route }) {
+  const storeData = route?.params?.storeData;
+  const getData = route?.params?.getData;
+  const [count, setCount] = useState({ boolean: false });
+
+  const getDataInformation = async () => {
+    let value = await getData();
+    console.log(value);
+    setCount(value);
+  };
+
+  useEffect(() => {}, [count]);
+
+  console.log(count.boolean.toString());
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Image
-        source={{ uri: "https://via.placeholder.com/208x208/ccc" }}
-        style={{ width: 208, height: 208 }}
+      <Text>{"Questions"}</Text>
+      <Text>{count.boolean.toString()}</Text>
+      <Text>{"\n"}</Text>
+      <Button
+        title="Enter data true"
+        onPress={() => storeData({ boolean: true })}
       />
+      <Text>{"\n"}</Text>
+      <Button
+        title="Enter data false"
+        onPress={() => storeData({ boolean: false })}
+      />
+      <Text>{"\n"}</Text>
+      <Button title="Get data" onPress={() => getDataInformation()} />
+      <Text>{"\n"}</Text>
+      <Text>{"\n"}</Text>
       <Text>{"\n"}</Text>
       <Button
         title="Enter The Application"
