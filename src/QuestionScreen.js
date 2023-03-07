337eed88b5f8f8c927bfaf8fefa8e135ad643bed
Hans-Märten Liiu <hm4543@tlu.ee>
2023-03-07 14:27:48 +0200

    select for questions

--- src/QuestionScreen.js
+++ src/QuestionScreen.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Button, View, Image, Text } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import SingleQuestion from "./SingleQuestion";
 
 function StartScreen({ navigation, route }) {
   const storeData = route?.params?.storeData;
@@ -12,29 +14,103 @@ function StartScreen({ navigation, route }) {
     setCount(value);
   };
 
-  useEffect(() => {}, [count]);
+  const questionsList = [
+    "I recognize what I am feeling",
+    "When I am overcome with strong emotions I know how to manage them",
+    "question3",
+    "question4",
+    "I struggle to start working or studying without procrastinating",
+  ];
 
-  console.log(count.boolean.toString());
+  const [open, setOpen] = useState([false, false, false, false, false]);
+  const [value, setValue] = useState([null, null, null, null, null]);
+  const [items, setItems] = useState([
+    [
+      { label: "Apple", value: "apple" },
+      { label: "Banana", value: "banana" },
+    ],
+    [
+      { label: "Apple", value: "apple" },
+      { label: "Banana", value: "banana" },
+    ],
+    [
+      { label: "Apple", value: "apple" },
+      { label: "Banana", value: "banana" },
+    ],
+    [
+      { label: "Apple", value: "apple" },
+      { label: "Banana", value: "banana" },
+    ],
+    [
+      { label: "Apple", value: "apple" },
+      { label: "Banana", value: "banana" },
+    ],
+  ]);
+
+  useEffect(() => {
+    console.log(open);
+    console.log(value);
+    console.log(items);
+  }, [open]);
+
+  console.log(open);
+
+  const setOpenHandler = (i) => (e) => {
+    console.log(i);
+    console.log(e);
+
+    const newObject = [...open];
+    newObject[i] = e;
+    console.log(newObject);
+    setOpen(newObject);
+  };
+
+  const setValueHandler = (i) => (e) => {
+    console.log(i);
+    console.log(e);
+
+    const newObject = [...value];
+    newObject[i] = e;
+    console.log(newObject);
+    setValue(newObject);
+  };
+
+  const setItemsHandler = (i) => (e) => {
+    console.log(i);
+    console.log(e);
+
+    // const newObject = [...value];
+    // newObject[i] = e;
+    // console.log(newObject);
+    // //setOpen(newObject);
+  };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>{"Questions"}</Text>
-      <Text>{count.boolean.toString()}</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "lightblue",
+      }}
+    >
+      {questionsList.map((questionData, i) => (
+        <div key={i}>
+          <SingleQuestion
+            question={questionData}
+            open={open[i]}
+            value={value[i]}
+            items={items[i]}
+            setOpen={setOpenHandler(i)}
+            setValue={setValueHandler(i)}
+            setItems={setItemsHandler(i)}
+          ></SingleQuestion>
+        </div>
+      ))}
+
       <Text>{"\n"}</Text>
-      <Button
-        title="Enter data true"
-        onPress={() => storeData({ boolean: true })}
-      />
       <Text>{"\n"}</Text>
-      <Button
-        title="Enter data false"
-        onPress={() => storeData({ boolean: false })}
-      />
-      <Text>{"\n"}</Text>
-      <Button title="Get data" onPress={() => getDataInformation()} />
-      <Text>{"\n"}</Text>
-      <Text>{"\n"}</Text>
-      <Text>{"\n"}</Text>
+
       <Button
         title="Enter The Application"
         onPress={() => navigation.navigate("Bottomtab")}
---
+++ src/Style/DropDownPicker.js
@@ -0,0 +1,14 @@
+export const dropDownPickerStyle = {
+  width: "250px",
+  alignSelf: "center",
+};
+
+export const dropDownContainerStyle = {
+  backgroundColor: "#dfdfdf",
+  alignSelf: "center",
+  width: "250px",
+};
+
+export const selectedItemLabelStyle = {
+  fontWeight: "bold",
+};
